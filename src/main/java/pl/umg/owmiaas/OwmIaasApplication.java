7ebfd4081fe37420e94772423f176617617307f4
Tomasz Muchowski <tomasz_muchowski@protonmail.com>
2021-05-24 09:28:45 +0200

    Init

---
+++ src/main/java/pl/umg/owmiaas/OwmIaasApplication.java
@@ -0,0 +1,13 @@
+package pl.umg.owmiaas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwmIaasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwmIaasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/umg/owmiaas/OwmIaasApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.umg.owmiaas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwmIaasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
